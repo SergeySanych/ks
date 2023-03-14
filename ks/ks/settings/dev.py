452c2b5744e2848a323219c12acd6227f87d1c00
SergeySanych <sk@i-fabrika.kz>
2023-03-14 15:58:05 +0600

    14-03-2023-Создана база данных, установлен вагтейл

---
+++ ks/ks/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f$tv+8k+!)p2vd$8%9_$_6ptm&wp*jzgqj25=y-x^fmft9odx*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
